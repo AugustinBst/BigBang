26e373c86a6387200fbeb283b41fb4625a705877
Augustin Bost <augustin.bost@epitech.eu>
2026-02-05 13:16:58 +0200

    fix(failure on pupose to change with command after that)

--- bigbangarea/middleware.ts
+++ bigbangarea/middleware.ts
@@ -6,6 +6,6 @@ export default withAuth({
   },
 });
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+export const config = { 
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"] 
 };
